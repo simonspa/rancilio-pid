8e5def02470e8bb4fabdd687e6a38d91e7fe445b
Simon Spannagel <simonspa@kth.se>
2024-02-24 19:29:14 +0100

    Document Relay class

--- src/hardware/Relay.h
+++ src/hardware/Relay.h
@@ -5,21 +5,49 @@
  */
 #pragma once
 
+// Forward declaration of GPIOPin class
 class GPIOPin;
 
+/**
+ * \class Relay control class
+ * \brief This class provides control for relay switches
+ */
 class Relay {
     public:
 
+        /**
+         * \enum TriggerType
+         * \brief Type of trigger for this relay
+         * \details Relays can either trigger in HIGH or LOW setting of their control input
+         */
         enum TriggerType {
             LOW_TRIGGER,
             HIGH_TRIGGER
         };
 
+        /**
+         * \brief Constructor
+         *
+         * \param gpioInstance GPIO pin this relay is connected to
+         * \param trigger Trigger type this relay requires
+         */
         Relay(GPIOPin& gpioInstance, TriggerType trigger = HIGH_TRIGGER);
 
-        void on();
-        void off();
-        GPIOPin& getGPIOInstance();
+        /**
+         * \brief Switch relay on
+         */
+        void on() const;
+
+        /**
+         * \brief Switch relay off
+         */
+        void off() const;
+
+        /**
+         * \brief Get the GPIO pin this relay is connected to
+         * \return GPIO pin of the relay
+         */
+        GPIOPin& getGPIOInstance() const;
 
     private:
         GPIOPin& gpio;
